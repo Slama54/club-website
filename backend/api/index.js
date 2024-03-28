44c6b62ea93d3e0bb7efa9b9a7c5cba71bcbc8fa
wael <wael.slama.issat@gmail.com>
2024-03-28 02:56:49 +0200

    connect to the database

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log('dfz',process.env)
+mongoose.connect(process.env.MONGO).then(
+    ()=>{
+        console.log('mongoDb is connected')
+    }
+).catch((err)=>{
+    console.log(err)
+})
 const app = express();
 app.listen(3000, ()=>{
     console.log('server is running on port 3000 !!')
