0ead925a2d00c04c874e08399af903d52ac41c12
wael <wael.slama.issat@gmail.com>
2024-04-16 13:54:23 +0100

    Complete search page

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -15,6 +15,7 @@ import CreatePost from "./pages/CreatePost"
 import UpdatePost from "./pages/UpdatePost"
 import PostPage from "./pages/PostPage"
 import ScrollToTop from "./components/ScrollToTop"
+import Search from "./pages/Search"
 
 
 export default function App() {
@@ -31,6 +32,7 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />}/>
 
         <Route path="/sign-in" element={<SignIn />}/>
+        <Route path="/search" element={<Search />}/>
 
         <Route element={<PrivateRoute/>}>
 
